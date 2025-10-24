c46c319560d4fa9d5dd5aad0d8cec7353f66733a
juliosolis1 <julio.solis1@utp.ac.pa>
2025-10-23 23:02:57 -0500

    Comentarios colocados

--- AreaPerimetro.java
+++ AreaPerimetro.java
@@ -1,3 +1,26 @@
+/**
+Universidad Tecnológica de Panamá
+Facultad de Ingeniería en Sistemas Computacionales
+Licenciatura en Desarrollo y Gestión de Software
+
+Asignatura - Desarrollo de Software II
+
+Asignación #3 - Programación con Java
+
+Facilitador: Paulo Picota
+
+Integrantes:
+Diego Hernández | 20-70-8359
+Alanis Martez | 8-1019-2389
+Julio Solís | 8-1011-1457
+
+Grupo: 1GS117
+
+Fecha de entrega: 27 de octubre de 2025
+II Semestre | I Año
+**/
+
+// Problema #5 - Área y Perímetro
 public class AreaPerimetro {
     
 }
